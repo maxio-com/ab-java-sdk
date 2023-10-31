bf3c473ae14a3cdd26124ce8ec9c4f999662256e
maxio-sdk <maxio-sdk@maxio.com>
2023-10-31 13:01:56 +0000

    Fix to spec file (createCustomer response for 422 HTTP code)

---
+++ doc/models/customer-error-response-exception.md
@@ -0,0 +1,23 @@
+
+# Customer Error Response Exception
+
+## Structure
+
+`CustomerErrorResponseException`
+
+## Fields
+
+| Name | Type | Tags | Description | Getter | Setter |
+|  --- | --- | --- | --- | --- | --- |
+| `Errors` | [`CustomerErrorResponseErrors`](../../doc/models/containers/customer-error-response-errors.md) | Optional | This is a container for any-of cases. | CustomerErrorResponseErrors getErrors() | setErrors(CustomerErrorResponseErrors errors) |
+
+## Example (as JSON)
+
+```json
+{
+  "errors": {
+    "customer": "customer8"
+  }
+}
+```
+
---
+++ src/main/java/com/maxio/advancedbilling/exceptions/CustomerErrorResponseException.java
@@ -0,0 +1,49 @@
+/*
+ * AdvancedBilling
+ *
+ * This file was automatically generated for Maxio by APIMATIC v3.0 ( https://www.apimatic.io ).
+ */
+
+package com.maxio.advancedbilling.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.maxio.advancedbilling.models.Errors;
+import com.maxio.advancedbilling.models.containers.CustomerErrorResponseErrors;
+import io.apimatic.coreinterfaces.http.Context;
+
+/**
+ * This is a model class for CustomerErrorResponseException type.
+ */
+public class CustomerErrorResponseException extends ApiException {
+    private static final long serialVersionUID = -18428866054026842L;
+    private CustomerErrorResponseErrors errors;
+
+    /**
+     * Initialization constructor.
+     * @param   reason  The reason for throwing exception
+     * @param   context The context of the API exception
+     */
+    public CustomerErrorResponseException(String reason, Context context) {
+        super(reason, context);
+    }
+
+
+    /**
+     * Getter for Errors.
+     * @return Returns the CustomerErrorResponseErrors
+     */
+    @JsonGetter("errors")
+    public CustomerErrorResponseErrors getErrors() {
+        return this.errors;
+    }
+
+    /**
+     * Setter for Errors.
+     * @param errors Value for CustomerErrorResponseErrors
+     */
+    @JsonSetter("errors")
+    private void setErrors(CustomerErrorResponseErrors errors) {
+        this.errors = errors;
+    }
+}
