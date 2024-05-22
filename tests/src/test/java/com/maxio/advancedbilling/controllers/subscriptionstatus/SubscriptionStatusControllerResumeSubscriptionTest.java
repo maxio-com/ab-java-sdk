363b3634a52072e09475d565bc76dec4db8b70fc
Maciej Nedza <maciej.nedza@keen.io>
2024-05-22 08:31:37 +0200

    [DE-827] Exclude property from test

--- tests/src/test/java/com/maxio/advancedbilling/controllers/subscriptionstatus/SubscriptionStatusControllerResumeSubscriptionTest.java
+++ tests/src/test/java/com/maxio/advancedbilling/controllers/subscriptionstatus/SubscriptionStatusControllerResumeSubscriptionTest.java
@@ -29,7 +29,7 @@ public class SubscriptionStatusControllerResumeSubscriptionTest extends Subscrip
         // then
         assertThat(resumedSubscription).usingRecursiveComparison()
                 .ignoringFields("updatedAt", "prepaidDunning", "previousState", "productPricePointType",
-                        "dunningCommunicationDelayEnabled", "customer.countryName", "customer.stateName")
+                        "dunningCommunicationDelayEnabled", "customer.countryName", "customer.stateName", "customer.locale")
                 .isEqualTo(subscription);
         assertThat(resumedSubscription.getUpdatedAt()).isAfter(timestamp);
         assertThat(resumedSubscription.getState()).isEqualTo(ACTIVE);
