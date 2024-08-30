7bdd5d994bfe1c0d10f95b011dabf7c2be0d1237
maxio-sdk <maxio-sdk@maxio.com>
2024-08-30 15:20:58 +0000

    Automated commit message

--- src/main/java/com/maxio/advancedbilling/AdvancedBillingClient.java
+++ src/main/java/com/maxio/advancedbilling/AdvancedBillingClient.java
@@ -98,7 +98,7 @@ public final class AdvancedBillingClient implements Configuration {
 
     private static final CompatibilityFactory compatibilityFactory = new CompatibilityFactoryImpl();
 
-    private static String userAgent = "AB SDK Java:5.0.0 on OS {os-info}";
+    private static String userAgent = "AB SDK Java:5.0.1 on OS {os-info}";
 
     /**
      * Current API environment.
