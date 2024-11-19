9d83036b48875b3cdefb265137b694bcd88d99ab
Patryk Grudzień <59444374+patryk-grudzien-keen@users.noreply.github.com>
2024-11-19 09:15:44 +0100

    [DE-544] Subscription fixes (#166)
    - added `SubscriptionResponse` for `purgeSubscription` operation
    - `purgeSubscription` now throws `SubscriptionResponseErrorException`
    - `updatePrepaidSubscriptionConfiguration` now throws `ApiException` when the status code is 422

---
+++ src/main/java/com/maxio/advancedbilling/exceptions/SubscriptionResponseErrorException.java
@@ -0,0 +1,48 @@
+/*
+ * AdvancedBilling
+ *
+ * This file was automatically generated for Maxio by APIMATIC v3.0 ( https://www.apimatic.io ).
+ */
+
+package com.maxio.advancedbilling.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.maxio.advancedbilling.models.Subscription;
+import io.apimatic.coreinterfaces.http.Context;
+
+/**
+ * This is a model class for SubscriptionResponseErrorException type.
+ */
+public class SubscriptionResponseErrorException extends ApiException {
+    private static final long serialVersionUID = -53087963905887759L;
+    private Subscription subscription;
+
+    /**
+     * Initialization constructor.
+     * @param   reason  The reason for throwing exception
+     * @param   context The context of the API exception
+     */
+    public SubscriptionResponseErrorException(String reason, Context context) {
+        super(reason, context);
+    }
+
+
+    /**
+     * Getter for Subscription.
+     * @return Returns the Subscription
+     */
+    @JsonGetter("subscription")
+    public Subscription getSubscription() {
+        return this.subscription;
+    }
+
+    /**
+     * Setter for Subscription.
+     * @param subscription Value for Subscription
+     */
+    @JsonSetter("subscription")
+    private void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+}
